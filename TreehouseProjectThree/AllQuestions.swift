bf6a791761394849d9c0d92dbad2a6b66abe0b27
Danilo Chaves <chaves_danilo@emerson.edu>
2016-09-14 21:05:46 -0400

    Project Beginning

---
+++ TreehouseProjectThree/AllQuestions.swift
@@ -0,0 +1,9 @@
+//
+//  AllQuestions.swift
+//  TreehouseProjectThree
+//
+//  Created by Danilo E Chaves on 9/14/16.
+//  Copyright © 2016 Danilo E Chaves. All rights reserved.
+//
+
+import Foundation
