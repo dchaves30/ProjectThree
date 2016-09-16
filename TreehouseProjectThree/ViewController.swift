404199a9e75ba9a850ba8c527087a11b73e9ea55
Danilo Chaves <chaves_danilo@emerson.edu>
2016-09-15 22:59:42 -0400

    Day Two

--- TreehouseProjectThree/ViewController.swift
+++ TreehouseProjectThree/ViewController.swift
@@ -10,16 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var downButtonOne: UIButton!
+    @IBOutlet weak var upButtonTwo: UIButton!
+    @IBOutlet weak var downButtonTwo: UIButton!
+    @IBOutlet weak var upButtonThree: UIButton!
+    @IBOutlet weak var downButtontThree: UIButton!
+    @IBOutlet weak var upButtonFour: UIButton!
+    @IBOutlet weak var nextRoundButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadButtons()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
---
+++ buttonActions.swift
@@ -0,0 +1,34 @@
+//
+//  buttonActions.swift
+//  TreehouseProjectThree
+//
+//  Created by Danilo E Chaves on 9/15/16.
+//  Copyright © 2016 Danilo E Chaves. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension ViewController {
+
+    
+//Receive a button name and a Image name and set the button states to pushed or not pushed values
+    func buttonImage(notPushed:UIImage, pushed:UIImage, buttonName:UIButton) {
+        
+        buttonName.setImage(notPushed, for: .normal)
+        buttonName.setImage(pushed, for: .highlighted)
+        
+    }
+
+//load all the button images including the button states when the app starts in viewDidLoad()
+    func loadButtons() {
+        buttonImage(notPushed: #imageLiteral(resourceName: "down_full"), pushed: #imageLiteral(resourceName: "down_full_selected"), buttonName: downButtonOne)
+        buttonImage(notPushed: #imageLiteral(resourceName: "down_half"), pushed: #imageLiteral(resourceName: "down_half_selected"), buttonName: downButtonTwo)
+        buttonImage(notPushed: #imageLiteral(resourceName: "up_half"), pushed: #imageLiteral(resourceName: "up_half_selected"), buttonName: upButtonTwo)
+        buttonImage(notPushed: #imageLiteral(resourceName: "down_half"), pushed: #imageLiteral(resourceName: "down_half_selected"), buttonName: downButtontThree)
+        buttonImage(notPushed: #imageLiteral(resourceName: "up_half"), pushed: #imageLiteral(resourceName: "up_half_selected"), buttonName: upButtonThree)
+        buttonImage(notPushed: #imageLiteral(resourceName: "up_full"), pushed: #imageLiteral(resourceName: "up_full_selected"), buttonName: upButtonFour)
+    }
+}
+
+
